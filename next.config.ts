965dfba507c58403908e384f2aaa22482c0b1351
Robel seife <robelseife972@gmail.com>
2026-04-01 20:19:33 +0300

    fix: add playwright-core and update serverExternalPackages to fix Playwright missing error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: [
+    "playwright",
+    "playwright-core",
     "playwright-extra",
     "puppeteer-extra",
     "puppeteer-extra-plugin-stealth",
